4bdf036c98c34ffc44665a248756dc8e3ad047c1
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-10-14 17:30:37 +0700

    update foto

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,81 +1,85 @@
-import dark1 from "./dark1.svg";
-import dark2 from "./dark2.svg";
-import light1 from "./light1.svg";
-import light2 from "./light2.svg";
-import copyright from "./copyright.svg";
-import email1 from "./email1.svg";
-import email2 from "./email2.svg";
-import figma1 from "./figma1.svg";
-import figma2 from "./figma2.svg";
-import github1 from "./github1.svg";
-import github2 from "./github2.svg";
-import lokasi1 from "./lokasi1.svg";
-import lokasi2 from "./lokasi2.svg";
-import telepon1 from "./telepon1.svg";
-import telepon2 from "./telepon2.svg";
-import twitter1 from "./twitter1.svg";
-import twitter2 from "./twitter2.svg";
-import tujuan1 from "./tujuan1.svg";
-import expressjs from "./expressjs.svg";
-import figma from "./figma.svg";
-import git from "./git.svg";
-import javascript from "./javascript.svg";
-import mongo from "./mongo.svg";
-import nextjs from "./nextjs.svg";
-import nodejs from "./nodejs.svg";
-import postgresql from "./postgresql.svg";
-import rjs from "./rjs.svg";
-import tailwindcss from "./tailwindcss.svg";
-import typescript from "./typescript.svg";
-import titik5 from "./titik5.svg";
-import titik1 from "./titik1.svg";
-import x from "./x.svg";
-import berandacmn from "./berandacmn.png";
-import berandacfi from "./berandacfi.png";
-import logincmn from "./logincmn.png";
-import logincfi from "./logincfi.png";
-import berandacprint from "./berandacprint.png";
-import logo from "./logo.png";
-import alex from "./alex.jpg";
+/** @format */
+
+import dark1 from './dark1.svg';
+import dark2 from './dark2.svg';
+import light1 from './light1.svg';
+import light2 from './light2.svg';
+import copyright from './copyright.svg';
+import email1 from './email1.svg';
+import email2 from './email2.svg';
+import figma1 from './figma1.svg';
+import figma2 from './figma2.svg';
+import github1 from './github1.svg';
+import github2 from './github2.svg';
+import lokasi1 from './lokasi1.svg';
+import lokasi2 from './lokasi2.svg';
+import telepon1 from './telepon1.svg';
+import telepon2 from './telepon2.svg';
+import twitter1 from './twitter1.svg';
+import twitter2 from './twitter2.svg';
+import tujuan1 from './tujuan1.svg';
+import expressjs from './expressjs.svg';
+import figma from './figma.svg';
+import git from './git.svg';
+import javascript from './javascript.svg';
+import mongo from './mongo.svg';
+import nextjs from './nextjs.svg';
+import nodejs from './nodejs.svg';
+import postgresql from './postgresql.svg';
+import rjs from './rjs.svg';
+import tailwindcss from './tailwindcss.svg';
+import typescript from './typescript.svg';
+import titik5 from './titik5.svg';
+import titik1 from './titik1.svg';
+import x from './x.svg';
+import berandacmn from './berandacmn.png';
+import berandacfi from './berandacfi.png';
+import logincmn from './logincmn.png';
+import logincfi from './logincfi.png';
+import berandacprint from './berandacprint.png';
+import logo from './logo.png';
+import alex from './alex.jpg';
+import alex2 from './alex2.jpg';
 
 export {
-  alex,
-  logo,
-  logincfi,
-  logincmn,
-  berandacfi,
-  berandacmn,
-  berandacprint,
-  rjs,
-  x,
-  titik5,
-  titik1,
-  expressjs,
-  git,
-  figma,
-  javascript,
-  mongo,
-  nextjs,
-  nodejs,
-  postgresql,
-  tailwindcss,
-  typescript,
-  dark1,
-  dark2,
-  light1,
-  light2,
-  copyright,
-  email1,
-  email2,
-  figma1,
-  figma2,
-  github1,
-  github2,
-  lokasi1,
-  lokasi2,
-  telepon1,
-  telepon2,
-  twitter1,
-  twitter2,
-  tujuan1,
+	alex,
+	logo,
+	logincfi,
+	logincmn,
+	berandacfi,
+	berandacmn,
+	berandacprint,
+	rjs,
+	x,
+	titik5,
+	titik1,
+	expressjs,
+	git,
+	figma,
+	javascript,
+	mongo,
+	nextjs,
+	nodejs,
+	postgresql,
+	tailwindcss,
+	typescript,
+	dark1,
+	dark2,
+	light1,
+	light2,
+	copyright,
+	email1,
+	email2,
+	figma1,
+	figma2,
+	github1,
+	github2,
+	lokasi1,
+	lokasi2,
+	telepon1,
+	telepon2,
+	twitter1,
+	twitter2,
+	tujuan1,
+	alex2,
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,83 +1,101 @@
-import React from "react";
-import { email1, email2, github1, titik1, lokasi1, alex } from "../assets";
+/** @format */
+
+import React from 'react';
+import {
+	email1,
+	email2,
+	github1,
+	titik1,
+	lokasi1,
+	alex,
+	alex2,
+} from '../assets';
 
 const handleGithub = () => {
-  const fileUrl = "https://github.com/Alexchandrahanif";
-  window.open(fileUrl, "_blank");
+	const fileUrl = 'https://github.com/Alexchandrahanif';
+	window.open(fileUrl, '_blank');
 };
 const Profile = () => {
-  return (
-    <div className="w-full  pt-[70px] px-4  dark:bg-dark" id="profile">
-      <div className=" w-full py-8 px-2 flex flex-col sm:flex-row-reverse sm:px-12 gap-12">
-        {/*  */}
-        <div className="sm:px-8">
-          <div className="w-full h-[300px] flex justify-center items-center sm:w-[500px] ">
-            <img
-              src={alex}
-              alt=""
-              class="object-cover"
-              className="w-[300px] h-[300px] rounded-lg"
-            />
-          </div>
-        </div>
-        <div className="w-full   flex flex-col gap-12">
-          <div className="flex flex-col gap-2">
-            <p className="text-3xl font-poppins font-semibold dark:text-textDark2 sm:text-4xl">
-              Hi, I'm Alex 👋
-            </p>
-            <p className="text-slate-800 text-sm dark:text-textDark2 sm:text-lg">
-              I'm a Fullstack JavaScript Developer with a specialization in
-              backend development, complemented by frontend proficiency. Excels
-              in building robust server-side applications, designing efficient
-              APIs, and implementing databases. skilled in crafting captivating
-              and user-centric frontend interfaces using HTML, CSS, and React
-            </p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <div className="flex gap-1">
-              <img src={lokasi1} alt="" />
-              <p className="text-sm dark:text-textDark2 sm:text-lg">
-                Jakarta, Indonesia
-              </p>
-            </div>
-            <div className="flex gap-1">
-              <img src={titik1} alt="" />
-              <p className="text-sm dark:text-textDark2 sm:text-lg">
-                Available for New Project
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-2 ">
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                handleGithub();
-              }}
-            >
-              <img src={github1} alt="github" />
-            </button>
-            <button>
-              <a
-                href="https://www.linkedin.com/in/alex-chandra-hanif-aa9955236/"
-                target="_blank"
-              >
-                <svg
-                  width="20px"
-                  class="fill-current"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 dark:bg-slate-600"
-                >
-                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-                </svg>
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="w-full  pt-[70px] px-4  dark:bg-dark"
+			id="profile">
+			<div className=" w-full py-8 px-2 flex flex-col sm:flex-row-reverse sm:px-12 gap-12">
+				{/*  */}
+				<div className="sm:px-8">
+					{/* <div className="w-full h-[300px] flex justify-center items-center sm:w-[500px] ">
+						<img
+							src={alex2}
+							alt=""
+							class="object-cover"
+							className="w-[300px] h-[300px] rounded-lg"
+						/>
+					</div> */}
+				</div>
+				<div className="w-full   flex flex-col gap-12">
+					<div className="flex flex-col gap-2">
+						<p className="text-3xl font-poppins font-semibold dark:text-textDark2 sm:text-4xl">
+							Hi, I'm Alex 👋
+						</p>
+						<p className="text-slate-800 text-sm dark:text-textDark2 sm:text-lg">
+							I'm a Fullstack JavaScript Developer with a specialization in
+							backend development, complemented by frontend proficiency. Excels
+							in building robust server-side applications, designing efficient
+							APIs, and implementing databases. skilled in crafting captivating
+							and user-centric frontend interfaces using HTML, CSS, and React
+						</p>
+					</div>
+					<div className="flex flex-col gap-1">
+						<div className="flex gap-1">
+							<img
+								src={lokasi1}
+								alt=""
+							/>
+							<p className="text-sm dark:text-textDark2 sm:text-lg">
+								Jakarta, Indonesia
+							</p>
+						</div>
+						<div className="flex gap-1">
+							<img
+								src={titik1}
+								alt=""
+							/>
+							<p className="text-sm dark:text-textDark2 sm:text-lg">
+								Available for New Project
+							</p>
+						</div>
+					</div>
+					<div className="flex gap-2 ">
+						<button
+							className="cursor-pointer"
+							onClick={() => {
+								handleGithub();
+							}}>
+							<img
+								src={github1}
+								alt="github"
+							/>
+						</button>
+						<button>
+							<a
+								href="https://www.linkedin.com/in/alex-chandra-hanif-aa9955236/"
+								target="_blank">
+								<svg
+									width="20px"
+									class="fill-current"
+									role="img"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+									className="w-6 h-6 dark:bg-slate-600">
+									<path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+								</svg>
+							</a>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
